68f09b6a66c648d837168dc994ad99c353375ad9
Matt Mottle <mattmottle1@gmail.com>
2023-06-14 15:49:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'hello'>
+      <h1>Hello Dojo!</h1>
+      <ul>Things I need to do:</ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feed The Doggos</li>
     </div>
   );
 }
-
 export default App;
+
+
